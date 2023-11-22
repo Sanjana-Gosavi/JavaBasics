50d6f05d47903439c4d43eeb203d16c90ca94d3b
Sanjana-Gosavi <sanjgosavi0810@gmail.com>
2023-11-22 12:43:46 +0530

    String Exercise

--- JavaBasics/src/com/prowings/stringmethods/DuplicateRemoval.java
+++ JavaBasics/src/com/prowings/stringmethods/DuplicateRemoval.java
@@ -3,33 +3,29 @@ package com.prowings.stringmethods;
 public class DuplicateRemoval {
 
 	public static void main(String[] args) {
-		
+
 		String s = "Sanjana";
-		
-		System.out.println("Input String is:" +s);
-		
-		System.out.println("After removing duplicates string is:"+removeDuplicateChars(s));
+
+		System.out.println("Input String is:" + s);
+
+		System.out.println("After removing duplicates string is:" + removeDuplicateChars(s));
 	}
-	
-	
+
 	private static String removeDuplicateChars(String s) {
-		
+
 		char[] ch = s.toCharArray();
-		
-		for(int i = 0; i < s.length();i++)
-		{
-			for(int j = i+1; j < s.length(); j++)
-			{
+
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = i + 1; j < s.length(); j++) {
 				if (ch[i] == ch[j])
 					ch[j] = ' ';
 			}
 		}
-		
+
 		String res = new String(ch);
 		res = res.replaceAll(" ", "");
 		return res;
 	}
-
 
 	public DuplicateRemoval() {
 	}
--- JavaBasics/src/com/prowings/stringmethods/StringReverse.java
+++ JavaBasics/src/com/prowings/stringmethods/StringReverse.java
@@ -1,32 +1,29 @@
 package com.prowings.stringmethods;
 
 public class StringReverse {
-	
+
 	public static void main(String[] args) {
-		
+
 		String s1 = "HelloWorld";
-		System.out.println("Origional String is:" +s1);
-		
-		System.out.println("Reversed String is:" +reverseString(s1));
-		
+		System.out.println("Origional String is:" + s1);
+
+		System.out.println("Reversed String is:" + reverseString(s1));
+
 	}
 
 	public static String reverseString(String s1) {
-		
-		if(s1 == null)
-		return null;
-		
+
+		if (s1 == null)
+			return null;
+
 		String reverseString = "";
-		
-		for(int i = s1.length()-1;i>=0;i--) {
+
+		for (int i = s1.length() - 1; i >= 0; i--) {
 			reverseString += s1.charAt(i);
-		
+
 		}
-		
+
 		return reverseString;
 	}
-	
-	
-	
-	
+
 }
